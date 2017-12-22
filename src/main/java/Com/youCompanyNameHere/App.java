14f993b91604eeb13078565159820bec4ccb54ea
kkj0201 <kkj0201@nate.com>
2017-12-22 18:02:36 +0900

    1

--- src/main/java/Com/youCompanyNameHere/App.java
+++ src/main/java/Com/youCompanyNameHere/App.java
@@ -11,6 +11,8 @@ public class App
     public static void main( String[] args )
     {
         INDArray x = Nd4j.create(new double[] {1, 2, 3, 4, 5, 6}, new int[]{3, 2});
+        x.add(2);
+
         System.out.println(x);
 
         System.out.println( "Hello World!" );
